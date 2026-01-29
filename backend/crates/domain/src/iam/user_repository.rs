62630a28d2f2088ec8b51fae037618c86a88f165
benjschoen8 <benjschoen8@gmail.com>
2026-01-30 04:22:20 +0800

    chore: rename

--- backend/crates/domain/src/iam/user_repository.rs
+++ backend/crates/domain/src/iam/user_repository.rs
@@ -1,11 +1,11 @@
 use async_trait::async_trait;
 use crate::shared::user_id::UserId;
-use crate::iam::iam_user::IAMUser;
+use crate::iam::iam_user::IamUser;
 use crate::iam::iam_error::IamError;
 
 #[async_trait]
 pub trait UserRepository: Send + Sync {
-    async fn save(&self, user: &IAMUser) -> Result<(), IamError>;
-    async fn find_by_username(&self, username: &str) -> Result<Option<IAMUser>, IamError>;
-    async fn find_by_id(&self, id: UserId) -> Result<Option<IAMUser>, IamError>;
+    async fn save(&self, user: &IamUser) -> Result<(), IamError>;
+    async fn find_by_username(&self, username: &str) -> Result<Option<IanUser>, IamError>;
+    async fn find_by_id(&self, id: UserId) -> Result<Option<IanUser>, IamError>;
 }
