34bf4f60d092dbd6a8e1b057c93d6efa0ebe2c60
benjschoen8 <benjschoen8@gmail.com>
2026-01-30 16:06:17 +0800

    fix: obj reference correction

--- backend/crates/domain/src/iam/user_repository.rs
+++ backend/crates/domain/src/iam/user_repository.rs
@@ -6,6 +6,6 @@ use crate::iam::iam_error::IamError;
 #[async_trait]
 pub trait UserRepository: Send + Sync {
     async fn save(&self, user: &IamUser) -> Result<(), IamError>;
-    async fn find_by_username(&self, username: &str) -> Result<Option<IanUser>, IamError>;
-    async fn find_by_id(&self, id: UserId) -> Result<Option<IanUser>, IamError>;
+    async fn find_by_username(&self, username: &str) -> Result<Option<IamUser>, IamError>;
+    async fn find_by_id(&self, id: UserId) -> Result<Option<IamUser>, IamError>;
 }
