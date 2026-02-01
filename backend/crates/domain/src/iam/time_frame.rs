f214268e3b856c03c71bec10d185fac0c0463ac9
benjschoen8 <benjschoen8@gmail.com>
2026-02-01 10:09:28 +0800

    fix: spelling error

--- backend/crates/domain/src/iam/time_frame.rs
+++ backend/crates/domain/src/iam/time_frame.rs
@@ -3,11 +3,11 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct TimeFrame {
     pub issued: usize,
-    pub experation: usize,
+    pub expiration: usize,
 }
 
 impl TimeFrame {
-    pub fn new(issued: usize, experation: usize) -> Self {
-        Self { issued, experation}
+    pub fn new(issued: usize, expiration: usize) -> Self {
+        Self { issued, expiration}
     }
 }
