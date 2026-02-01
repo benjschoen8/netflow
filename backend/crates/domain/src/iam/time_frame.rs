a46f5f117416eda5ddba62034f5cc24b2bf277b7
benjschoen8 <benjschoen8@gmail.com>
2026-02-01 10:18:17 +0800

    refactor: make immutable

--- backend/crates/domain/src/iam/time_frame.rs
+++ backend/crates/domain/src/iam/time_frame.rs
@@ -1,13 +1,27 @@
 use serde::{Deserialize, Serialize};
+use chrono::{DateTime, Utc};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct TimeFrame {
-    pub issued: usize,
-    pub expiration: usize,
+    issued_at: DateTime<Utc>,
+    expires_at: DateTime<Utc>,
 }
 
 impl TimeFrame {
-    pub fn new(issued: usize, expiration: usize) -> Self {
-        Self { issued, expiration}
+    pub fn new(issued_at: DateTime<Utc>, duration: std::time::Duration) -> Self {
+        let expires_at = issued_at + chrono::Duration::from_std(duration).unwrap();
+        Self { issued_at, expires_at }
+    }
+    
+    pub fn issued_at(&self) -> DateTime<Utc> {
+        self.issued_at
+    }
+
+    pub fn expires_at(&self) -> DateTime<Utc> {
+        self.expires_at
+    }
+
+    pub fn is_expired(&self) -> bool {
+        Utc::now() > self.expires_at
     }
 }
